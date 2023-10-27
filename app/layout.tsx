3684c8b1d2e88ead71c9f121204687bed35c031a
Rajnish Pratap Singh <125359327+rajnishramaera@users.noreply.github.com>
2023-10-27 13:04:41 +0530

    fc

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Inter, Roboto_Mono, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+  display: "swap",
+});
+const jetbrains_mono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Rama Setu",
+  description: "Rama Bridge",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={` dark ${inter.variable} ${roboto_mono.variable} ${jetbrains_mono.variable}`}
+    >
+      <body className=" font-jet">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ page.tsx
@@ -0,0 +1,10 @@
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+
+export default function Home() {
+  return (
+    <main>
+      <div>hus</div>
+      <ThemeSwitcher />
+    </main>
+  );
+}
