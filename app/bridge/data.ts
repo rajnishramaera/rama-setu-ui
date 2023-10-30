0d7c7576259c12715933bde6e8361c76e42b4d63
Rajnish Pratap Singh <125359327+rajnishramaera@users.noreply.github.com>
2023-10-30 17:35:44 +0530

    select

---
+++ app/bridge/data.ts
@@ -0,0 +1,26 @@
+export const tokens = [
+  {
+    id: "rama",
+    name: "Ramaestta",
+    icon: "/network/rama.svg",
+    moreDetils: "Ramaestta Chain",
+  },
+  {
+    id: "bnb",
+    name: "BNB Smart Chain",
+    icon: "/network/bnb.svg",
+    moreDetils: "BNB Smart Chain",
+  },
+  {
+    id: "eth",
+    name: "Ethereum",
+    icon: "/network/eth.svg",
+    moreDetils: "Ethereum",
+  },
+  {
+    id: "polygon",
+    name: "Polygon",
+    icon: "/network/polygon.svg",
+    moreDetils: "Polygon",
+  },
+]
