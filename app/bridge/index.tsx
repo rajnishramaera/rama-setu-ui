044a4e15ba7c0b7e5415236cc39507b7fad9e03f
Rajnish Pratap Singh <125359327+rajnishramaera@users.noreply.github.com>
2023-10-31 16:43:48 +0530

    border card

--- app/bridge/index.tsx
+++ app/bridge/index.tsx
@@ -3,7 +3,7 @@ import SelectToken from "./components/selectToken"
 const Bridge = () => {
   return (
     <div className="flex items-center h-full">
-      <div className="mx-auto lg:w-[30%] bg-gradient-to-tr from-violet-500 to-orange-300 dark:bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] dark:from-sky-400 dark:to-indigo-900 p-4 rounded-medium">
+      <div className="mx-auto border-2 dark:border-gray-600 border-gray-300 lg:w-[30%] bg-gradient-to-tr from-violet-500 to-orange-300 dark:bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] dark:from-sky-400 dark:to-indigo-900 p-4 rounded-medium">
         <SelectToken />
         <SelectToken />
         <SelectToken />
