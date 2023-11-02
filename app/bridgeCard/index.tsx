48a4c228320812a341f55d464fd12c1ce89ee5c1
Rajnish Pratap Singh <125359327+rajnishramaera@users.noreply.github.com>
2023-11-02 13:22:04 +0530

    rm plugin

--- app/bridgeCard/index.tsx
+++ app/bridgeCard/index.tsx
@@ -19,15 +19,22 @@ const SlideOne = () => (
 
 const BridgeCard = () => {
   const [mounted, setMounted] = useState(false)
-
   useEffect(() => {
     setMounted(true)
   }, [])
 
+  setTimeout(function () {
+    if (!mounted) {
+      console.log("first")
+    }
+  }, 5000)
+
   return (
     <div className="flex h-full items-center">
       {mounted ? (
-        <div className=" mx-auto space-y-6  rounded-medium border-2 border-gray-300 bg-gradient-to-tr from-violet-500 to-orange-300 p-6 dark:border-gray-600 dark:bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] dark:from-sky-400 dark:to-indigo-900 lg:w-[30%]">
+        <div
+          className=" mx-auto space-y-6  
+        rounded-medium border-2 border-gray-300 bg-gradient-to-tr from-violet-500 to-orange-300 p-6 dark:border-gray-600 dark:bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] dark:from-sky-400 dark:to-indigo-900 lg:w-[30%]">
           <SlideOne />
         </div>
       ) : (
