c653b4c03f978b4d6086b8f5e877d6926212fc97
Rajnish Pratap Singh <125359327+rajnishramaera@users.noreply.github.com>
2023-11-02 15:21:39 +0530

    circles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,40 @@ import "./globals.css"
 import { ThemeProviders } from "./_providers/themeProviders"
 import { Web3Providers } from "./_providers/web3Providers"
 
+const Circles = (
+  <ul className="circles">
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+    <li>
+      <img className="invert dark:invert-0" src="/network/rama.svg" />
+    </li>
+  </ul>
+)
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -37,38 +71,7 @@ export default function RootLayout({
       lang="en"
       className={` dark ${inter.variable} ${roboto_mono.variable} ${jetbrains_mono.variable} px-2`}>
       <body className=" font-jet area px-4">
-        <ul className="circles">
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>{" "}
-          <li>
-            <img className="invert dark:invert-0" src="/network/rama.svg" />
-          </li>
-        </ul>
+        {Circles}
         <ThemeProviders>
           <Web3Providers>
             <Header />
